5e950b4e728596ed465a2d229df08d39484a6c75
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-17 19:55:19 -0400

    FourOFour.jsx: imported useNavigate | created an h1 header |  added an image element | created a button and added onclick navigate to home.jsx

--- src/pages/FourOFour.jsx
+++ src/pages/FourOFour.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const FourOFour = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>404: Page Not Found</h1>
+            <img id='FourOFour-image' src="" alt="fourOfour-img" />
+            <button onClick={() => navigate('/')}>Home</button>
         </div>
     );
 };
