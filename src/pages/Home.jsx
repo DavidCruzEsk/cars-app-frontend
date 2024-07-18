9f09ed93d6ccbb25fb9f932ad0bff692a34c473e
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-17 21:21:14 -0400

    Inside App.css: added classNames: car-section-header, cars-header, home-container, car-portrait, car-header-div, make-header, model-header| added CSS styling to each

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,21 +15,26 @@ const Home = () => {
 
     // when marco pushes Humps, convert the cars variables into camelCase.
     return (
-        <div>
-            Home Page
-            {cars.map((car) => {
-                const { id, make, model, img_url } = car;
-                return (
-                    <div className='car-portrait' key={id}>
-                        <img src={img_url} alt='car-photo' width='300px' height='200px'/>
-                        <div className='car-header-div'>
-                            <h3>{make}</h3>
-                            <h2><i>{model}</i></h2>
+        <>
+            <header className='car-selection-header'>
+                <h1 className='cars-header'>Cars</h1>
+                <h3 className='cars-selection-link'>Link</h3>
+            </header>
+            <div className='home-container'>
+                {cars.map((car) => {
+                    const { id, make, model, img_url } = car;
+                    return (
+                        <div className='car-portrait' key={id}>
+                            <img src={img_url} alt='car-photo' width='200px' height='125px'/>
+                            <div className='car-header-div'>
+                                <h3 className='make-header'>{make}</h3>
+                                <h2 className='model-header'><i>{model}</i></h2>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
-        </div>
+                    );
+                })}
+            </div>
+        </>
     );
 };
 
