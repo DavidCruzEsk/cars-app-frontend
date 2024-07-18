4c113e5d0cf601e5fe5e44b7c28add70001ee698
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-17 20:27:29 -0400

    On home.jsx: imported axios for fetching | imported useEffect and useState | imported useNavigate | fetched all cars from backend | mapped cars into a div and placed the image, make and model of each car into its own div | place the make and model values into a div of headers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const [cars, setCars] = useState([]);
+    const API = import.meta.env.VITE_BASE_URL;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`${API}`)
+            .then(res => setCars(res.data))
+            .catch(err => navigate('*'))
+    }, []);
+
+    // when marco pushes Humps, convert the cars variables into camelCase.
     return (
         <div>
             Home Page
+            {cars.map((car) => {
+                const { id, make, model, img_url } = car;
+                return (
+                    <div className='car-portrait' key={id}>
+                        <img src={img_url} alt='car-photo' width='300px' height='200px'/>
+                        <div className='car-header-div'>
+                            <h3>{make}</h3>
+                            <h2><i>{model}</i></h2>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
